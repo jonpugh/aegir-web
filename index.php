21f595787e7f4878ea8a23583aef02f2aaae10a7
Jon Pugh <jon@thinkdrop.net>
2016-10-17 13:17:02 -0400

    adding our own index.php

---
+++ index.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * THIS FILE IS A COPY FROM DRUPAL CORE.
+ *
+ * The only difference is the autoload.php file is in vendor.
+ * All other files are in ./core and ./vendor.
+ */
+
+/**
+ * @file
+ * The PHP page that serves all page requests on a Drupal installation.
+ *
+ * All Drupal code is released under the GNU General Public License.
+ * See COPYRIGHT.txt and LICENSE.txt files in the "core" directory.
+ */
+
+use Drupal\Core\DrupalKernel;
+use Symfony\Component\HttpFoundation\Request;
+
+$autoloader = require_once 'vendor\autoload.php';
+
+$kernel = new DrupalKernel('prod', $autoloader);
+
+$request = Request::createFromGlobals();
+$response = $kernel->handle($request);
+$response->send();
+
+$kernel->terminate($request, $response);
